7f8d69ace46b7bb4280027352a3915f5452d083a
sana.kobayashi <sa09na21@gmail.com>
2021-12-24 14:37:40 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_082134) do
+ActiveRecord::Schema.define(version: 2021_12_24_142742) do
 
   create_table "messages", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
